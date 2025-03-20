aaeb28c08ed35a08f2070847641919bf26045b29
dschifel <danielschifelbain@gmail.com>
2025-03-20 18:48:23 -0300

    New: Application and default test

---
+++ src/test/java/com/danielks/MarketList/MarketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielks.MarketList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
