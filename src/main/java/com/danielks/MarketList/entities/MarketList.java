c2ab22b5ec0386d4d9e634bf6f786060f5ea87e3
dschifel <danielschifelbain@gmail.com>
2025-03-20 18:44:31 -0300

    Created: Market List class/entity

---
+++ src/main/java/com/danielks/MarketList/entities/MarketList.java
@@ -0,0 +1,62 @@
+package com.danielks.MarketList.entities;
+
+import com.danielks.MarketList.utils.MarketItemConverter;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_market_list")
+public class MarketList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    @Convert(converter = MarketItemConverter.class)
+    private List<MarketItem> items;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    private String description;
+
+    private Float totalValue;
+
+    public MarketList() {
+    }
+
+    public MarketList(UUID id, List<MarketItem> items, LocalDateTime createDate, String description, Float totalValue) {
+        this.id = id;
+        this.items = items;
+        this.createDate = createDate == null ? LocalDateTime.now() : createDate;
+        this.description = description;
+        this.totalValue = totalValue;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public List<MarketItem> getItems() {
+        return items;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public Float getTotalValue() {
+        return totalValue;
+    }
+
+
+}
