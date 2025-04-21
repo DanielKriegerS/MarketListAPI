88a02530ff692a4b7c617c74ba46cd14e56df241
dschifel <danielschifelbain@gmail.com>
2025-04-21 13:08:04 -0300

    Fixed: static + api endpoints

--- src/main/java/com/danielks/MarketList/controllers/FinishedMarketListController.java
+++ src/main/java/com/danielks/MarketList/controllers/FinishedMarketListController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/finished-market-lists")
+@RequestMapping("/finished-market-lists")
 public class FinishedMarketListController {
 
     private final FinishedMarketListService service;
