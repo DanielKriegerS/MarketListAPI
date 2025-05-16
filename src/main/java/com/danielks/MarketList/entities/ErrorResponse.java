fb2107ff8e5c87e411b1e195c17cb446b133b761
dschifel <danielschifelbain@gmail.com>
2025-05-16 18:32:13 -0300

    V1_New: created error response class

---
+++ src/main/java/com/danielks/MarketList/entities/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.danielks.MarketList.entities;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+    private final int status;
+    private final String error;
+    private final String message;
+
+    public ErrorResponse(HttpStatus status, String message) {
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
