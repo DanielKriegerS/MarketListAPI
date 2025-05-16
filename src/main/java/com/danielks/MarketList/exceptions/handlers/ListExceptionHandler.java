c581c51e2ee93d19449d8de51234aa8d93d8c4fd
dschifel <danielschifelbain@gmail.com>
2025-05-16 18:37:40 -0300

    V1_New: created list exception handler

---
+++ src/main/java/com/danielks/MarketList/exceptions/handlers/ListExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.danielks.MarketList.exceptions.handlers;
+
+import com.danielks.MarketList.controllers.MarketListController;
+import com.danielks.MarketList.entities.ErrorResponse;
+import com.danielks.MarketList.entities.mappers.ErrorResponseMapper;
+import com.danielks.MarketList.exceptions.market_list.ListNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(assignableTypes = {MarketListController.class})
+public class ListExceptionHandler {
+    private final ErrorResponseMapper errorResponseMapper;
+
+    public ListExceptionHandler(ErrorResponseMapper errorResponseMapper) {
+        this.errorResponseMapper = errorResponseMapper;
+    }
+
+    @ExceptionHandler(ListNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleListNotFound(ListNotFoundException ex) {
+        ErrorResponse error = errorResponseMapper.toErrorResponse(HttpStatus.NOT_FOUND, ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
