b15a7585257626e309debfc1d29ab3480fb752c7
dschifel <danielschifelbain@gmail.com>
2025-05-19 17:55:11 -0300

    V1_Fixed: add cors config to patch methods

--- src/main/java/com/danielks/MarketList/config/CorsConfig.java
+++ src/main/java/com/danielks/MarketList/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200", "https://market-list-front-ruddy.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS")
                         .allowedHeaders("*");
             }
         };
