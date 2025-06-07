f2ea2dda65efe72242185c22b2e990de96bd57b1
dschifel <danielschifelbain@gmail.com>
2025-06-07 14:19:58 -0300

    V1_Refac.

--- src/main/java/com/danielks/MarketList/entities/dtos/ListSummaryDTO.java
+++ src/main/java/com/danielks/MarketList/entities/dtos/ListSummaryDTO.java
@@ -3,5 +3,10 @@ package com.danielks.MarketList.entities.dtos;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record ListSummaryDTO(UUID id, LocalDateTime date, String description, Float totalValue, int totalItems ,boolean isFinished) {
+public record ListSummaryDTO(UUID id,
+                             LocalDateTime date,
+                             String description,
+                             Float totalValue,
+                             int totalItems ,
+                             boolean isFinished) {
 }
