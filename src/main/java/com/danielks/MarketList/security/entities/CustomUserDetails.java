e5412cd85afc71301ce1767798f257d89d573858
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-26 20:26:58 -0300

    New: Created custom user details and implemented basic user details methods

---
+++ src/main/java/com/danielks/MarketList/security/entities/CustomUserDetails.java
@@ -0,0 +1,67 @@
+package com.danielks.MarketList.security.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class CustomUserDetails implements UserDetails {
+    private final UUID id;
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+    private final boolean enabled;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.enabled = true;
+        this.authorities = user.getRoles()
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+}
