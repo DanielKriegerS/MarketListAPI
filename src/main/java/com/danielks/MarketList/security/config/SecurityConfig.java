3c0bf8661e109bf15e786bb9ebb878f8c697defd
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-26 20:24:40 -0300

    New: Created security config + Implemented filter chain and auth manager

---
+++ src/main/java/com/danielks/MarketList/security/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.danielks.MarketList.security.config;
+
+import com.danielks.MarketList.security.filters.JwtAuthenticationFilter;
+import com.danielks.MarketList.security.services.UserService;
+import com.danielks.MarketList.security.utils.JwtUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    private final JwtUtils jwtUtils;
+    private final JwtAuthenticationFilter jwtFilter;
+    private final UserService userService;
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    public SecurityConfig(JwtUtils jwtUtils, JwtAuthenticationFilter jwtFilter, UserService userService, CorsConfigurationSource corsConfigurationSource) {
+        this.jwtUtils = jwtUtils;
+        this.jwtFilter = jwtFilter;
+        this.userService = userService;
+        this.corsConfigurationSource = corsConfigurationSource;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource)) // ativa CORS para Spring Security
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
