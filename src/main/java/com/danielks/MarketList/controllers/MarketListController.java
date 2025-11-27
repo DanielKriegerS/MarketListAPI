f99e3a063e65cb7fb35a68daf6ad541d9bfb0d05
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-27 13:50:14 -0300

    Feat/refac: implemented authentication on get open-lists and get finished lists endpoints / adjusted HATEOAS to reflect authentication

--- src/main/java/com/danielks/MarketList/controllers/MarketListController.java
+++ src/main/java/com/danielks/MarketList/controllers/MarketListController.java
@@ -28,41 +28,41 @@ public class MarketListController {
     }
 
     @GetMapping("/open-lists")
-    public ResponseEntity<CollectionModel<EntityModel<ListSummaryDTO>>> getOpenLists() {
-        List<ListSummaryDTO> openLists = service.getOpenLists();
+    public ResponseEntity<CollectionModel<EntityModel<ListSummaryDTO>>> getOpenLists(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<ListSummaryDTO> openLists = service.getOpenLists(userDetails.getId());
 
         List<EntityModel<ListSummaryDTO>> listModels = openLists.stream()
                 .map(EntityModel::of).toList();
 
         CollectionModel<EntityModel<ListSummaryDTO>> collectionModel = CollectionModel.of(listModels,
-                linkTo(methodOn(MarketListController.class).getOpenLists()).withSelfRel(),
-                linkTo(methodOn(MarketListController.class).getFinishedLists()).withRel("Finished Lists"));
+                linkTo(methodOn(MarketListController.class).getOpenLists(userDetails)).withSelfRel(),
+                linkTo(methodOn(MarketListController.class).getFinishedLists(userDetails)).withRel("Finished Lists"));
 
         return ResponseEntity.ok(collectionModel);
     }
 
     @GetMapping("/finished-lists")
-    public ResponseEntity<CollectionModel<EntityModel<ListSummaryDTO>>> getFinishedLists() {
-        List<ListSummaryDTO> finishedLists = service.getFinishedLists();
+    public ResponseEntity<CollectionModel<EntityModel<ListSummaryDTO>>> getFinishedLists(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<ListSummaryDTO> finishedLists = service.getFinishedLists(userDetails.getId());
 
         List<EntityModel<ListSummaryDTO>> listModels = finishedLists.stream()
                 .map(EntityModel::of).toList();
 
         CollectionModel<EntityModel<ListSummaryDTO>> collectionModel = CollectionModel.of(listModels,
-                linkTo(methodOn(MarketListController.class).getFinishedLists()).withSelfRel(),
-                linkTo(methodOn(MarketListController.class).getOpenLists()).withRel("Open Lists"));
+                linkTo(methodOn(MarketListController.class).getFinishedLists(userDetails)).withSelfRel(),
+                linkTo(methodOn(MarketListController.class).getOpenLists(userDetails)).withRel("Open Lists"));
 
         return ResponseEntity.ok(collectionModel);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EntityModel<CompleteListDTO>> getById(@PathVariable UUID id) {
+    public ResponseEntity<EntityModel<CompleteListDTO>> getById(@PathVariable UUID id, @AuthenticationPrincipal CustomUserDetails userDetails) {
         CompleteListDTO list = service.getById(id);
 
         EntityModel<CompleteListDTO> model = EntityModel.of(list,
-                linkTo(methodOn(MarketListController.class).getById(id)).withSelfRel(),
-                linkTo(methodOn(MarketListController.class).getOpenLists()).withRel("Open Lists"),
-                linkTo(methodOn(MarketListController.class).getFinishedLists()).withRel("Finished Lists")
+                linkTo(methodOn(MarketListController.class).getById(id, userDetails)).withSelfRel(),
+                linkTo(methodOn(MarketListController.class).getOpenLists(userDetails)).withRel("Open Lists"),
+                linkTo(methodOn(MarketListController.class).getFinishedLists(userDetails)).withRel("Finished Lists")
         );
         return ResponseEntity.ok(model) ;
     }
@@ -72,25 +72,25 @@ public class MarketListController {
         CompleteListDTO saved = service.create(marketList, userDetails.getId());
 
         EntityModel<CompleteListDTO> model = EntityModel.of(saved,
-                linkTo(methodOn(MarketListController.class).getById(saved.id())).withSelfRel(),
-                linkTo(methodOn(MarketListController.class).getOpenLists()).withRel("Open Lists"),
-                linkTo(methodOn(MarketListController.class).getFinishedLists()).withRel("Finished Lists")
+                linkTo(methodOn(MarketListController.class).getById(saved.id(), userDetails)).withSelfRel(),
+                linkTo(methodOn(MarketListController.class).getOpenLists(userDetails)).withRel("Open Lists"),
+                linkTo(methodOn(MarketListController.class).getFinishedLists(userDetails)).withRel("Finished Lists")
         );
 
         return ResponseEntity
                 .created(linkTo(methodOn(MarketListController.class)
-                        .getById(saved.id()))
+                        .getById(saved.id(), userDetails))
                         .toUri())
                 .body(model);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EntityModel<CompleteListDTO>> update(@PathVariable UUID id, @RequestBody MarketList updatedList) {
+    public ResponseEntity<EntityModel<CompleteListDTO>> update(@PathVariable UUID id, @RequestBody MarketList updatedList, @AuthenticationPrincipal CustomUserDetails userDetails) {
         CompleteListDTO list = service.update(id, updatedList);
         EntityModel<CompleteListDTO> model = EntityModel.of(list,
-                linkTo(methodOn(MarketListController.class).getById(id)).withSelfRel(),
-                linkTo(methodOn(MarketListController.class).getOpenLists()).withRel("Open Lists"),
-                linkTo(methodOn(MarketListController.class).getFinishedLists()).withRel("Finished Lists")
+                linkTo(methodOn(MarketListController.class).getById(id, userDetails)).withSelfRel(),
+                linkTo(methodOn(MarketListController.class).getOpenLists(userDetails)).withRel("Open Lists"),
+                linkTo(methodOn(MarketListController.class).getFinishedLists(userDetails)).withRel("Finished Lists")
         );
 
         return ResponseEntity.ok(model);
@@ -103,13 +103,13 @@ public class MarketListController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<EntityModel<FinishedListDTO>> finishList(@PathVariable UUID id) {
+    public ResponseEntity<EntityModel<FinishedListDTO>> finishList(@PathVariable UUID id, @AuthenticationPrincipal CustomUserDetails userDetails) {
         FinishedListDTO finishedList = service.finishList(id);
 
         EntityModel<FinishedListDTO> model = EntityModel.of(finishedList,
-                linkTo(methodOn(MarketListController.class).getById(id)).withSelfRel(),
-                linkTo(methodOn(MarketListController.class).getOpenLists()).withRel("Open Lists"),
-                linkTo(methodOn(MarketListController.class).getFinishedLists()).withRel("Finished Lists")
+                linkTo(methodOn(MarketListController.class).getById(id, userDetails)).withSelfRel(),
+                linkTo(methodOn(MarketListController.class).getOpenLists(userDetails)).withRel("Open Lists"),
+                linkTo(methodOn(MarketListController.class).getFinishedLists(userDetails)).withRel("Finished Lists")
         );
 
         return ResponseEntity.ok(model);
