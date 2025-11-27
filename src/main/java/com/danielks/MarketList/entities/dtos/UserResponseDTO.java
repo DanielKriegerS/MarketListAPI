5b89955ff01754bba99d4c3110ab91b06139eb66
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-27 18:54:07 -0300

    Fix: added attributes

--- src/main/java/com/danielks/MarketList/entities/dtos/UserResponseDTO.java
+++ src/main/java/com/danielks/MarketList/entities/dtos/UserResponseDTO.java
@@ -1,6 +1,13 @@
 package com.danielks.MarketList.entities.dtos;
 
-public record UserResponseDTO(
+import java.util.Set;
+import java.util.UUID;
 
+public record UserResponseDTO(
+        UUID id,
+        String username,
+        String password,
+        Set<String> roles,
+        String refreshToken
 ) {
 }
